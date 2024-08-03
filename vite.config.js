b08c6d3c7b97ee921de339fa23d31b4e03e99876
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-08-03 16:51:21 +0300

    ok

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	plugins: [vue()],
 	server: {
-		port: 3000, // Misol uchun, 3000 portini tanlang
+		port: 3010, // Misol uchun, 3000 portini tanlang
 		host: true, // yoki '0.0.0.0'
 	},
 })
